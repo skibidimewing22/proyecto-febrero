67ef36210e7155d07a064e5914bb314655485b85
skibidimewing22 <48588107@est.ort.edu.ar>
2026-02-22 00:22:28 -0300

    df

--- tito-calderon/js/productos.js
+++ tito-calderon/js/productos.js
@@ -1,23 +1,66 @@
 const productos = [
   {
     id: 1,
-    nombre: "Moka Clásica 3 tazas",
+    nombre: "Moka",
     precio: 18999,
-    imagen: "https://picsum.photos/300?1",
-    descripcion: "Cafetera italiana tradicional."
+    imagen: "img/moka.png",
+    descripcion: "Clásica cafetera italiana. Fuerte, simple y rápida.",
+    categoria: "Moka"
   },
   {
     id: 2,
-    nombre: "Moka 6 tazas",
-    precio: 24999,
-    imagen: "https://picsum.photos/300?2",
-    descripcion: "Ideal para más cantidad de café."
+    nombre: "Sifón",
+    precio: 54999,
+    imagen: "img/sifon.png",
+    descripcion: "Extracción limpia y aromática. Método llamativo.",
+    categoria: "Sifón"
   },
   {
     id: 3,
-    nombre: "Molino Manual",
-    precio: 15999,
-    imagen: "https://picsum.photos/300?3",
-    descripcion: "Molino ajustable para mejor molienda."
+    nombre: "Espresso",
+    precio: 249999,
+    imagen: "img/espresso.png",
+    descripcion: "Café intenso con crema. Estilo cafetería.",
+    categoria: "Espresso"
+  },
+  {
+    id: 4,
+    nombre: "Chemex",
+    precio: 45999,
+    imagen: "img/chemex.png",
+    descripcion: "Filtrado suave, limpio y muy aromático.",
+    categoria: "Chemex"
+  },
+  {
+    id: 5,
+    nombre: "De vacío",
+    precio: 69999,
+    imagen: "img/de_vacio.png",
+    descripcion: "Método de vacío con cuerpo medio y aroma alto.",
+    categoria: "De vacío"
+  },
+  {
+    id: 6,
+    nombre: "Prensa Francesa",
+    precio: 29999,
+    imagen: "img/prensa.png",
+    descripcion: "Más cuerpo y textura. Simple y rendidor.",
+    categoria: "Prensa Francesa"
+  },
+  {
+    id: 7,
+    nombre: "Turca",
+    precio: 21999,
+    imagen: "img/turca.png",
+    descripcion: "Tradición. Molienda fina y sabor potente.",
+    categoria: "Turca"
+  },
+  {
+    id: 8,
+    nombre: "Goteo V60",
+    precio: 38999,
+    imagen: "img/v60.png",
+    descripcion: "Goteo controlado. Perfil suave y balanceado.",
+    categoria: "Goteo V60"
   }
 ];
